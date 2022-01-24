9c43425e9175762c59f0354b3e3346dd665d42b9
minsu12345 <qnchalstn@gmail.com>
2022-01-24 12:41:22 +0900

    20220124

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //이동에 사용할 리지드바디 컴포넌트
+    public Rigidbody playerRigidbody;
+    //이동속력
+    public float speed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
