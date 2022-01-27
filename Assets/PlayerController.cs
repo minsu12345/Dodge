b129e72f3468fb3ab0e9fa97c1f493e95631376f
minsu12345 <qnchalstn@gmail.com>
2022-01-27 12:43:16 +0900

    20220127

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,12 +11,25 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+
+        playerRigidbody.velocity = newVelocity;
+
+    }
+    public void die()
+    {
+        gameObject.SetActive(false);
     }
 }
